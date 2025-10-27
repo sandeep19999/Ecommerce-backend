5d3e1e6cf547c5a180e3e240e821d4829c0fd977
sandeepanumula <anumulasandeep96@gmail.com>
2025-10-27 07:13:35 +0530

    added h2 database

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -1,6 +1,6 @@
 package com.example.ecommerce.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Product {
@@ -11,4 +11,27 @@ public class Product {
     private Double price;
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
